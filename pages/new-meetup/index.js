59b44511302c6af7114bf21c5235337d3b342e95
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-10-11 13:07:26 +0200

    Add nav bar and new meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,13 @@
 // our-domain.com/new-meetup
+import React from 'react';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+const NewMeetUpPage = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetUpPage;
